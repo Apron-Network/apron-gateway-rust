106766cf5a23a17eab1befc61d42d58d7d57af89
kuleleyokn42 <74556186+kuleleyokn42@users.noreply.github.com>
2021-11-28 12:49:51 +0800

    Migrate some common forward code to separated function

--- src/fwd_handlers.rs
+++ src/fwd_handlers.rs
@@ -1,5 +1,3 @@
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
 use std::collections::HashMap;
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
@@ -7,18 +5,33 @@ use std::sync::mpsc::{Receiver, Sender};
 use actix_web::web::Data;
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use actix_web_actors::ws;
-use log::debug;
-
 use futures::SinkExt;
+use log::debug;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use uuid::Bytes;
 
-use crate::forward_service_models::HttpProxyResponse;
+use crate::forward_service_models::{HttpProxyResponse, ProxyRequestInfo};
 use crate::helpers;
 use crate::network::Command;
 use crate::state::{set, AppState};
 use crate::{forward_service_actors, forward_service_utils::parse_request, PeerId, SharedHandler};
 
-// TODO: This should be invoked in service side gw
+fn prepare_for_sending_p2p_transaction(
+    query_args: web::Query<HashMap<String, String>>,
+    raw_body: web::Bytes,
+    req: HttpRequest,
+) -> (ProxyRequestInfo, PeerId) {
+    // Parse request from client side
+    let req_info = parse_request(query_args, raw_body, &req);
+
+    // Generate peer id from seed
+    // TODO: Replace this hard coded value to value fetched from service registration DB
+    let (_, remote_peer_id) = helpers::generate_peer_id_from_seed(Some(1));
+
+    (req_info, remote_peer_id)
+}
+
 pub(crate) async fn forward_http_proxy_request(
     query_args: web::Query<HashMap<String, String>>,
     raw_body: web::Bytes,
@@ -27,36 +40,33 @@ pub(crate) async fn forward_http_proxy_request(
     local_peer_id: Data<PeerId>,
     request_id_client_session_mapping: AppState<Sender<HttpProxyResponse>>,
 ) -> impl Responder {
-    println!("Local peer id: {:?}", local_peer_id);
+    debug!("ClientSideGateway: Receive HTTP request: {:?}", req);
 
-    // Parse request from client side
-    let req_info = parse_request(query_args, raw_body, &req);
+    let (req_info, remote_peer_id) = prepare_for_sending_p2p_transaction(query_args, raw_body, req);
 
-    // For p2p environment, the req_info should be sent to service side gateway via stream
-    let mut command_sender = p2p_handler.command_sender.lock().unwrap();
-
-    let (remote_key, remote_peer_id) = helpers::generate_peer_id_from_seed(Some(1));
-
-    println!("[fwd] Request info: {:?} to {}", &req_info, remote_peer_id);
+    debug!("ClientSideGateway: Req info: {:?}", req_info);
+    debug!("ClientSideGateway: remote peer: {:?}", remote_peer_id);
 
     let (resp_sender, resp_receiver): (Sender<HttpProxyResponse>, Receiver<HttpProxyResponse>) =
         mpsc::channel();
 
+    // Save mapping between request_id and response sender,
+    // which will be used to locate correct client session while getting response
     set(
         request_id_client_session_mapping.clone(),
         req_info.clone().request_id,
         resp_sender.clone(),
     );
 
-    println!(
-        "Fwd req id mapping: {:?}",
+    debug!(
+        "ClientSideGateway: Fwd req id mapping: {:?}",
         request_id_client_session_mapping.as_ref()
     );
 
     // Send ProxyRequestInfo to service side gateway via stream
+    let mut command_sender = p2p_handler.command_sender.lock().unwrap();
     command_sender
         .send(Command::SendRequest {
-            // peer: PeerId::from_str(local_peer_id.as_str()).unwrap(),
             peer: remote_peer_id,
             data: bincode::serialize(&req_info).unwrap(),
         })
@@ -70,21 +80,58 @@ pub(crate) async fn forward_http_proxy_request(
 
 pub(crate) async fn forward_ws_proxy_request(
     query_args: web::Query<HashMap<String, String>>,
+    raw_body: web::Bytes,
     req: HttpRequest,
     stream: web::Payload,
     p2p_handler: Data<SharedHandler>,
+    local_peer_id: Data<PeerId>,
+    request_id_client_session_mapping: AppState<Sender<HttpProxyResponse>>,
 ) -> impl Responder {
-    let req_info = parse_request(query_args, web::Bytes::new(), &req);
+    debug!("ClientSideGateway: Receive Ws request: {:?}", req);
+
+    let (req_info, remote_peer_id) = prepare_for_sending_p2p_transaction(query_args, raw_body, req);
+
     debug!("ClientSideGateway: Req info: {:?}", req_info);
-    // TODO: Change to configured ws server addr
-    let resp = ws::start(
-        forward_service_actors::ClientSideWsActor {
-            service_uri: "ws://localhost:10000",
-            addr: None,
-        },
-        &req,
-        stream,
+    debug!("ClientSideGateway: remote peer: {:?}", remote_peer_id);
+
+    let (resp_sender, resp_receiver): (Sender<HttpProxyResponse>, Receiver<HttpProxyResponse>) =
+        mpsc::channel();
+
+    // Save mapping between request_id and response sender,
+    // which will be used to locate correct client session while getting response
+    set(
+        request_id_client_session_mapping.clone(),
+        req_info.clone().request_id,
+        resp_sender.clone(),
     );
-    println!("Resp: {:?}", resp);
-    resp
+
+    debug!(
+        "ClientSideGateway: Fwd req id mapping: {:?}",
+        request_id_client_session_mapping.as_ref()
+    );
+
+    let mut command_sender = p2p_handler.command_sender.lock().unwrap();
+    command_sender
+        .send(Command::SendRequest {
+            peer: remote_peer_id,
+            data: bincode::serialize(&req_info).unwrap(),
+        })
+        .await
+        .unwrap();
+
+    let proxy_resp = resp_receiver.recv().unwrap();
+
+    HttpResponse::Ok().body(proxy_resp.body)
+
+    // // TODO: Change to configured ws server addr
+    // let resp = ws::start(
+    //     forward_service_actors::ClientSideWsActor {
+    //         service_uri: "ws://localhost:10000",
+    //         addr: None,
+    //     },
+    //     &req,
+    //     stream,
+    // );
+    // println!("Resp: {:?}", resp);
+    // resp
 }
