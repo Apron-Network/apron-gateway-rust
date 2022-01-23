3db2ec85de3cee52f72b455fea79311542b484e9
kuleleyokn42 <74556186+kuleleyokn42@users.noreply.github.com>
2022-01-23 23:41:30 +0800

    Add UsageReportManager class to manage usage report (still in dev)

--- src/usage_report.rs
+++ src/usage_report.rs
@@ -1,10 +1,36 @@
+use std::collections::HashMap;
 use std::ops::Deref;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+#[derive(Clone)]
+pub struct UsageReportManager {
+    pub account_reports: HashMap<String, UsageReport>,
+}
+
+impl UsageReportManager {
+    // harvest collect data from all recorded accounts, clear all records and wait for new round of accounting
+    fn harvest(&self) {}
+
+    fn add_record(&mut self, account_id: String, data_size: u128, is_upload: bool) {
+        let report = match self.account_reports.get(&account_id.clone()) {
+            Some(report) => *report.clone(),
+            None => {
+                let report = UsageReport::new(account_id.clone());
+                self.account_reports[&account_id.clone()] = report.clone();
+                report
+            }
+        };
+
+        report.inc_count(1);
+        report.inc_traffic(data_size, is_upload);
+    }
+}
+
+#[derive(Clone)]
 pub struct UsageReport {
     pub account_id: String,
     pub start_timestamp: u128,
-    pub end_timestamp:u128,
+    pub end_timestamp: u128,
     pub access_count: u32,
     pub upload_traffic: u128,
     pub download_traffic: u128,
@@ -12,27 +38,33 @@ pub struct UsageReport {
 
 impl UsageReport {
     fn new(account_id: String) -> UsageReport {
-        let current_ts = SystemTime::now().duration_since(UNIX_EPOCH).expect("Clock may have gone backwards").as_micros();
-        return UsageReport{
+        let current_ts = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Clock may have gone backwards")
+            .as_micros();
+        return UsageReport {
             account_id,
             start_timestamp: current_ts,
             end_timestamp: current_ts,
             access_count: 0,
             upload_traffic: 0,
-            download_traffic: 0
-        }
+            download_traffic: 0,
+        };
     }
 
     fn finalize(mut self) -> UsageReport {
-        self.end_timestamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Clock may have gone backwards").as_micros();
+        self.end_timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Clock may have gone backwards")
+            .as_micros();
         self
     }
 
     fn inc_count(mut self, cnt: u32) {
-        self.access_count+=cnt;
+        self.access_count += cnt;
     }
 
-    fn inc_traffic(mut self, pack_size: u128, is_upload: bool)  {
+    fn inc_traffic(mut self, pack_size: u128, is_upload: bool) {
         if is_upload {
             self.upload_traffic += pack_size;
         } else {
@@ -40,4 +72,3 @@ impl UsageReport {
         }
     }
 }
-
